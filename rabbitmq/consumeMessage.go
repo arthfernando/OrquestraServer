4cc2d9d0302820996fc5037ce560bc04aaee1e16
Arthur <arthur.fernando03@gmail.com>
2025-03-30 12:44:41 -0300

    rabbitmq publish and consume functions

---
+++ rabbitmq/consumeMessage.go
@@ -0,0 +1,60 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"log"
+	"painellembretes/config"
+	"painellembretes/models"
+	"painellembretes/shared"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func ConsumeMessage() {
+	rabbitmqUrl := config.Get("RABBITMQ_URL")
+	conn, err := amqp.Dial(rabbitmqUrl)
+	shared.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	shared.FailOnError(err, "Failed to open RabbitMQ channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"painellembretes-server",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	shared.FailOnError(err, "Failed to declare RabbitMQ queue")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	shared.FailOnError(err, "Failed to register RabbitMQ consumer")
+
+	consumerRoutine := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			var reminder models.Reminder
+
+			if err = json.Unmarshal(d.Body, &reminder); err != nil {
+				log.Printf("[ERR] Decoding message: %s", err.Error())
+				return
+			}
+			log.Printf("Received message: %s", reminder)
+		}
+	}()
+
+	log.Println("[*] Waiting messages. Press CTRL+C to exit.")
+	<-consumerRoutine
+}
---
+++ rabbitmq/sendMessage.go
@@ -0,0 +1,53 @@
+package rabbitmq
+
+import (
+	"encoding/json"
+	"log"
+	"painellembretes/config"
+	"painellembretes/models"
+	"painellembretes/shared"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func SendMessage(reminder models.Reminder) error {
+	rabbitmqUrl := config.Get("RABBITMQ_URL")
+
+	conn, err := amqp.Dial(rabbitmqUrl)
+	shared.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	shared.FailOnError(err, "Failed to open RabbitMQ channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"painellembretes-server",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	shared.FailOnError(err, "Failed to declare RabbitMQ queue")
+
+	encodedBody, err := json.Marshal(reminder)
+	if err != nil {
+		log.Println("Failed to encode body")
+		return err
+	}
+
+	err = ch.Publish(
+		"",
+		q.Name,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(encodedBody),
+		},
+	)
+	shared.FailOnError(err, "Failed to publish message")
+	log.Printf("[x] Message sent: %s", reminder)
+	return nil
+}
